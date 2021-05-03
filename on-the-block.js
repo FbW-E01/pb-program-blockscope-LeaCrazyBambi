1b824e4db1e7976988ca24a6bd6d4ff3d448f6a9
LeaCrazyBambi <lheynitz@gmail.com>
2021-05-03 14:19:17 +0200

    :D

---
+++ on-the-block.js
@@ -0,0 +1,21 @@
+
+let coconutsDeterminer = 0;
+let x = 8;
+
+if (coconutsDeterminer < 0) {
+    console.log(x = "Less than 0");
+} if (coconutsDeterminer >= 0) {
+   console.log("Greater or equal to 0")
+}
+
+// 2. New Variables
+let updater = 9;
+
+
+if ( coconutsDeterminer >= 0 ) {
+    updater = "Greater or equal to zero";
+
+    const message = "Positive Integer";
+    console.log(message)
+}
+
